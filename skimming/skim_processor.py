7ba2d40dff26b32d7696668729c47c518e861395
arttrip <162640219+arttrip@users.noreply.github.com>
2025-07-17 15:09:03 +0300

    remove fatjets from  skim_processor.py

--- skimming/skim_processor.py
+++ skimming/skim_processor.py
@@ -114,38 +114,7 @@ class NanoAODSkimmer(processor.ProcessorABC):
                     # Step 5: Recompute pt_regressed using filtered raw_pt and corrections
                     pt_regressed = raw_pt * pnet_cor * pnet_cor_net
                     collection["pt_regressed"] = pt_regressed
-            elif obj == "FatJet":
-                if hasattr(events, "FatJet"):
-                    #collection = events.FatJet
-                    collection = collection[(collection.pt > 100) & (abs(collection.eta) < 2.5)]
-                    eta_f = abs(collection.eta)
-                    chMult = collection.chMultiplicity
-                    neMult = collection.neMultiplicity
-                    neHEF = collection.neHEF
-                    neEmEF = collection.neEmEF
-                    chHEF = collection.chHEF
-                    muEF = getattr(collection, "muEF", ak.zeros_like(eta_f))
-                    chEmEF = getattr(collection, "chEmEF", ak.zeros_like(eta_f))
-                
-                    passJetIdTight = (
-                        ((eta_f <= 2.6) & (neHEF < 0.99) & (neEmEF < 0.9) & ((chMult + neMult) > 1) & (chHEF > 0.01) & (chMult > 0))
-                        |
-                        ((eta_f > 2.6) & (eta_f <= 2.7) & (neHEF < 0.90) & (neEmEF < 0.99))
-                        |
-                        ((eta_f > 2.7) & (eta_f <= 3.0) & (neHEF < 0.99))
-                        |
-                        ((eta_f > 3.0) & (neMult >= 2) & (neEmEF < 0.4))
-                    )
-                    
-                    passJetIdTightLepVeto = ak.where(
-                        eta_f <= 2.7,
-                        passJetIdTight & (muEF < 0.8) & (chEmEF < 0.8),
-                        passJetIdTight
-                    )
-                
-                    collection["passJetIdTight"] = passJetIdTight
-                    collection["passJetIdTightLepVeto"] = passJetIdTightLepVeto
-                    #out[obj] = self.select_fields(collection, self.branches_to_keep[obj])
+           
             out[obj] = self.select_fields(collection, self.branches_to_keep[obj])
         return out
 
