d3cbb7685ccb60a28478852d496e0c6fe4b55785
arttrip <162640219+arttrip@users.noreply.github.com>
2025-07-19 13:51:48 +0300

    updates to run the is_MVA  to make trees for BDT
    udates in processor, driver and source  scripts

--- analysis/ZH_0lep_total_processor.py
+++ analysis/ZH_0lep_total_processor.py
@@ -6,6 +6,7 @@ import argparse
 from coffea import processor
 from coffea.analysis_tools import Weights
 import hist
+from collections import defaultdict
 from coffea.nanoevents.methods import vector
 from hist import Hist
 import coffea.util
@@ -28,6 +29,21 @@ from utils.variables_def import (
 from utils.jet_tight_id import compute_jet_id
 import itertools
 
+def safe_array(array, target_len, fill_value=np.nan):
+    array = ak.fill_none(array, fill_value)
+    np_array = ak.to_numpy(array)
+
+    if np.isscalar(np_array):
+        np_array = np.full(target_len, fill_value)
+
+    if len(np_array) == target_len:
+        return np_array
+    elif len(np_array) < target_len:
+        pad_width = target_len - len(np_array)
+        return np.concatenate([np_array, np.full(pad_width, fill_value)])
+    else:
+        return np_array[:target_len]
+
 def make_vector(obj):
     return ak.zip({
         "pt": obj.pt,
@@ -44,18 +60,11 @@ def make_vector_met(met):
         "eta": ak.zeros_like(met.pt),
         "mass": ak.zeros_like(met.pt)
     },with_name="PtEtaPhiMLorentzVector", behavior=vector.behavior)
-    
+
 def build_sum_vector(jets, mask, n):
     '''
     Constructs a summed Lorentz vector from the first `n` jets per event **after applying a mask**.
     
-    Parameters:
-        jets: awkward.Array  -- nested jagged array of jets
-        mask: awkward.Array  -- boolean mask (same length as jets)
-        n: int               -- number of jets to sum (first n jets per event)
-
-    Returns:
-        LorentzVector awkward.Array: summed vector per event
     '''
     jets_masked = jets[mask]
     
@@ -67,12 +76,13 @@ def build_sum_vector(jets, mask, n):
     return vec_sum
 
 class TOTAL_Processor(processor.ProcessorABC):
-    def __init__(self, xsec=0.89, lumi=41.5e3, nevts=3000, isMC=False, dataset_name=None):
+    def __init__(self, xsec=0.89, nevts=3000, isMC=False, dataset_name=None, is_MVA=True):
         self.xsec = xsec
-        self.lumi = lumi
         self.nevts = nevts
         self.dataset_name = dataset_name
         self.isMC = isMC
+        self.is_MVA= is_MVA
+        self._trees = {regime: defaultdict(list) for regime in ["boosted", "resolved", "merged"]} if is_MVA else None
         self._histograms = {}
 
         self._histograms["cutflow_2l"] = hist.Hist(
@@ -131,6 +141,13 @@ class TOTAL_Processor(processor.ProcessorABC):
     def histograms(self):
         return self._histograms
 
+    def add_tree_entry(self, regime, data_dict):
+        if not self._trees or regime not in self._trees:
+            return
+        for key, val in data_dict.items():
+            self._trees[regime][key].extend(np.atleast_1d(val))
+        
+
     def process(self, events):
         try:
             events = events.eager_compute_divisions()
@@ -144,7 +161,14 @@ class TOTAL_Processor(processor.ProcessorABC):
             events = events.compute()
             n = len(events)
 
-        weight_array = np.ones(n) * (self.lumi * self.xsec / self.nevts)
+        is_signal = (
+            (self.dataset_name.startswith("ZH_ZToAll_HToAATo4B") or
+             self.dataset_name.startswith("WH_WToAll_HToAATo4B") and self.is_MC)
+        )
+        #for bdt train label
+        label_value = 1 if is_signal else 0
+
+        weight_array = np.ones(n) * ( self.xsec / self.nevts)
         weights = Weights(n)
         weights.add("norm", weight_array)
         output = {key: hist if not hasattr(hist, "copy") else hist.copy() for key, hist in self._histograms.items()}
@@ -160,7 +184,7 @@ class TOTAL_Processor(processor.ProcessorABC):
         n_leptons = ak.num(leptons)
        
         # Decide whether to compute Jet ID (XROOTD) or use existing branches (from skimmed file on eos)
-        if self.dataset_name and self.dataset_name.startswith("ZH_ZToAll_HToAATo4B"):
+        if is_signal:
             # Compute on the fly
             tight_id, tight_lep_veto = compute_jet_id(events.Jet)
             
@@ -214,14 +238,14 @@ class TOTAL_Processor(processor.ProcessorABC):
         double_bjets_boosted = double_bjets[full_mask_double]
         double_jets_boosted = double_jets[full_mask_double]
         double_untag_jets_boosted = double_untag_jets[full_mask_double]
-
+        
         n_untagged_boo = ak.num(double_untag_jets[full_mask_double])
         output["n_untag_boosted"].fill(n=n_untagged_boo, weight=weights_boosted)
         met_boosted = events.PuppiMET[full_mask_double]
 
         ht_boosted = ak.sum(double_jets_boosted.pt, axis=1)
         output["HT_boosted"].fill(ht=ht_boosted, weight=weights_boosted)
-
+        puppimet_boo= events.PuppiMET[full_mask_double]
         
         btag_max_boosted=double_bjets_boosted[:, 0].btagUParTAK4probbb
         btag_min_boosted=double_bjets_boosted[:, 1].btagUParTAK4probbb
@@ -248,9 +272,31 @@ class TOTAL_Processor(processor.ProcessorABC):
             dphi=lead_untag_boosted[valid_mask_boo].delta_phi(met_boosted[valid_mask_boo]),
             weight=weights_boosted[valid_mask_boo]
         )
+      
+        n_boost = len(weights_boosted)
+        bdt_boosted = {
+            "H_mass": safe_array(higgs_boost.mass, n_boost),
+            "H_pt": safe_array(higgs_boost.pt, n_boost),
+            "HT": safe_array(ht_boosted, n_boost),
+            "puppimet_pt": safe_array(puppimet_boo.pt, n_boost),
+            "btag_max": safe_array(btag_max_boosted, n_boost),
+            "btag_min": safe_array(btag_min_boosted, n_boost),
+            "dr_bb_ave": safe_array(dr_bb_avg(double_bjets_boosted), n_boost),
+            "dm_bb_bb_min": safe_array(dm_boosted, n_boost),
+            "dphi_H_MET": safe_array(np.abs(higgs_boost.delta_phi(met_boosted)), n_boost),
+            "pt_untag_max": safe_array(lead_untag_boosted[valid_mask_boo].pt, n_boost),
+            "dphi_untag_MET": safe_array(
+                np.abs(lead_untag_boosted[valid_mask_boo].delta_phi(met_boosted[valid_mask_boo])), n_boost
+            ),
+            "n_untag": safe_array(n_untagged_boo, n_boost),
+            "label": np.full(n_boost, label_value),
+            "weight": ak.to_numpy(weights_boosted),
+        }
 
+        if self.is_MVA:
+            self.add_tree_entry("boosted", bdt_boosted)
 
-
+       
         #resolved
         # For resolved regime
         full_mask_res = mask0 & (n_single_bjets >= 3)
@@ -362,7 +408,39 @@ class TOTAL_Processor(processor.ProcessorABC):
             dphi=first_untagged[valid_mask].delta_phi(puppimet_res[valid_mask]),
             weight=weights_res[valid_mask]
         )
+        n_res = len(weights_res)
+        bdt_resolved = {
+            "H_mass": safe_array(vec_H_res.mass, n_res),
+            "H_pt": safe_array(vec_H_res.pt, n_res),
+            "HT": safe_array(ak.sum(single_jets_resolved.pt, axis=1), n_res),
+            "dphi_H_MET": safe_array(
+                np.abs(vec_H_res.delta_phi(make_vector_met(puppimet_all_res))), n_res
+            ),
+            "dm_bb_bb_min": safe_array(
+                min_dm_bb_bb(vec_single_bjets_resolved, all_jets=vec_single_jets_resolved),
+                n_res
+            ),
+            "dr_bb_bb_ave": safe_array(
+                dr_bb_bb_avg(vec_single_bjets_resolved, all_jets=vec_single_jets_resolved),
+                n_res
+            ),
+            "m_bbj": safe_array(mbbj_resolved[valid_mask_mbbj], n_res),
+            "puppimet_pt": safe_array(puppimet_res.pt, n_res),
+            "n_untag": safe_array(ak.num(single_untag_jets_resolved), n_res),
+            "btag_max": safe_array(ak.max(single_bjets_resolved.btagDeepFlavB, axis=1), n_res),
+            "btag_min": safe_array(ak.min(single_bjets_resolved.btagDeepFlavB, axis=1), n_res),
+            "pt_untag_max": safe_array(pt_max_untagged_res[valid], n_res),
+            "dr_bb_ave": safe_array(dr_bb_avg(single_bjets_resolved), n_res),
+            "dphi_untag_MET": safe_array(
+                np.abs(first_untagged[valid_mask].delta_phi(puppimet_res[valid_mask])),
+                n_res
+            ),
+            "label": np.full(n_res, label_value),
+            "weight": ak.to_numpy(weights_res),
+        }
 
+        if self.is_MVA:
+            self.add_tree_entry("resolved", bdt_resolved)
         #merged
         full_mask_merged = mask0 & ((n_double_bjets >= 1) & (n_single_bjets_cc >= 1))
         output["cutflow_2l"].fill(cut="merged", weight=np.sum(weights.weight()[full_mask_merged]))
@@ -414,7 +492,7 @@ class TOTAL_Processor(processor.ProcessorABC):
 
         
         vec_H_merged= make_vector(vec_H_merged)
-        output["dphi_H_MET_merged"].fill(dphi=vec_H_merged.delta_phi(make_vector_met(puppimet_all)),weight=weights_merged_all)
+        output["dphi_H_MET_merged"].fill(dphi=np.abs(ak.to_numpy(vec_H_merged.delta_phi(make_vector_met(puppimet_all)))),weight=weights_merged_all)
 
         # Other variables that don’t depend on Higgs candidate
         
@@ -449,7 +527,7 @@ class TOTAL_Processor(processor.ProcessorABC):
         
         # dphi histogram
         output["dphi_untag_MET_merged"].fill(
-            dphi=ak.to_numpy(first_untag_merged[valid_mask_merged].delta_phi(puppimet_merged[valid_mask_merged])),
+            dphi=np.abs(ak.to_numpy(first_untag_merged[valid_mask_merged].delta_phi(puppimet_merged[valid_mask_merged]))),
             weight=ak.to_numpy(weights_merged[valid_mask_merged])
         )
 
@@ -472,8 +550,45 @@ class TOTAL_Processor(processor.ProcessorABC):
             dm=min_dm_bb_bb(make_vector(single_bjets_merged), all_jets=make_vector(single_jets_merged)),
             weight=weights_merged
         )
+        n_merged = len(weights_merged)
+        bdt_merged = {
+            "H_mass": safe_array(vec_H_merged.mass, n_merged),
+            "H_pt": safe_array(vec_H_merged.pt, n_merged),
+            "HT": safe_array(ht_total, n_merged),
+            "puppimet_pt": safe_array(puppimet_merged.pt, n_merged),
+            "n_untag": safe_array(ak.num(single_untag_jets_merged), n_merged),
+            "pt_untag_max": safe_array(first_untag_merged[valid_mask_merged].pt, n_merged),
+            "dphi_untag_MET": safe_array(
+                np.abs(first_untag_merged[valid_mask_merged].delta_phi(puppimet_merged[valid_mask_merged])),
+                n_merged,
+            ),
+            "btag_max": safe_array(ak.max(single_bjets_merged.btagDeepFlavB, axis=1), n_merged),
+            "btag_min": safe_array(ak.min(single_bjets_merged.btagDeepFlavB, axis=1), n_merged),
+            "dr_bb_ave": safe_array(dr_bb_avg(single_bjets_merged), n_merged),
+            "dm_bb_bb_min": safe_array(
+                min_dm_bb_bb(make_vector(single_bjets_merged), all_jets=make_vector(single_jets_merged)),
+                n_merged
+            ),
+            "dr_bb_bb_ave": safe_array(
+                dr_bb_bb_avg(make_vector(single_bjets_merged), all_jets=make_vector(single_jets_merged)),
+                n_merged
+            ),
+            "dphi_H_MET": safe_array(
+                np.abs(vec_H_merged.delta_phi(make_vector_met(puppimet_all))), n_merged
+            ),
+            "n_doubleb": safe_array(n_dbb, n_merged),
+            "n_singleb": safe_array(n_sbb, n_merged),
+            "label": np.full(n_merged, label_value),
+            "weight": ak.to_numpy(weights_merged),
+        }
+        if self.is_MVA:
+            self.add_tree_entry("merged", bdt_merged)
 
-        return output
-
+        if self.is_MVA:
+            output["trees"] = self._trees
+            for regime, trees in self._trees.items():
+                print(f"[DEBUG] Regime '{regime}' has {len(trees)} entries")
+        return output 
     def postprocess(self, accumulator):
+      
         return accumulator
