459564a0a1a0f83d5a21dc9bde694ad5c63457ae
arttrip <162640219+arttrip@users.noreply.github.com>
2025-08-15 14:13:24 +0300

    ADD some more 0-lep/2-lep triggers

--- skimming/skim_config.py
+++ skimming/skim_config.py
@@ -22,11 +22,11 @@ trigger_groups = {
     1: ["Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass8"],
     2: ["IsoMu24"],
     3: ["Mu50"],
-    4: ["Ele23_Ele12_CaloIdL_TrackIdL_IsoVL","DoubleEle25_CaloIdL_MW"],
+    4: ["Ele23_Ele12_CaloIdL_TrackIdL_IsoVL","Ele32_WPTight_Gsf"],
     5: ["Ele30_WPTight_Gsf"],
     6: ["Ele32_WPTight_Gsf"],
     7: ["Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ", "Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL", "Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ"],
-    8: ["PFMET120_PFMHT120_IDTight","PFMET120_PFMHT120_IDTight_PFHT60"],
+    8: ["PFMET120_PFMHT120_IDTight","PFMET120_PFMHT120_IDTight_PFHT60","PFMETNoMu120_PFMHTNoMu120_IDTight","PFMETNoMu120_PFMHTNoMu120_IDTight_FilterHF", "PFMETNoMu120_PFMHTNoMu120_IDTight_PFHT60"],
     9: ["QuadPFJet105_88_76_15"],
     10: ["DoublePFJets116MaxDeta1p6_DoubleCaloBTagDeepCSV_p71"],
     11: ["PFHT330PT30_QuadPFJet_75_60_45_40_PNet3BTag_2p0"],
@@ -42,6 +42,7 @@ trigger_groups = {
     21: ["QuadPFJet111_90_80_15_PNet2BTag_0p4_0p12_VBF1"],
     22: ["QuadPFJet111_90_80_15_PNetBTag_0p4_VBF2"],
     23: ["PFHT340_QuadPFJet70_50_40_40_PNet2BTagMean0p70"],
+    24: ["BTagMu_AK4DiJet20_Mu5"],        
 }
 
 
